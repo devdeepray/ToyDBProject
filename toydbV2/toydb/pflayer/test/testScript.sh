4d195534e30c95fad9052ea4a4189b87bf245ac3
devdeepray <devdeep.ray1994@iitb.ac.in>
2014-11-24 20:40:00 +0530

    SOme changes to script

--- toydbV2/toydb/pflayer/test/testScript.sh
+++ toydbV2/toydb/pflayer/test/testScript.sh
@@ -7,22 +7,30 @@ do
     echo "0 1 f2_"$i" "$i | ../createtable 2>/dev/null 1>/dev/null;
 done
 
-for i in {0..3}
-do
-    for j in {1..20}
-    do
-        echo $i" 2 f1_"$j | ../createtable 2>/dev/null 1>>"select_"$i".stats";
-    done
-done
+#for i in {0..3}
+#do
+#    for j in {1..20}
+#    do
+#        echo $i" 2 f1_"$j | ../createtable 2>/dev/null 1>>"select_"$i".stats";
+#    done
+#done
+
+#for i in {0..3}
+#do
+#    for j in {1..20}
+#    do
+#        echo $j;
+#        for k in {1..20}
+#        do
+#            echo $i" 3 f1_"$j" f2_"$k | ../createtable 2>/dev/null 1>>"join_"$i".stats";
+#        done
+#    done
+#done
 
 for i in {0..3}
 do
-    for j in {1..20}
+    for j in {0..20}
     do
-        echo $j;
-        for k in {1..20}
-        do
-            echo $i" 3 f1_"$j" f2_"$k | ../createtable 2>/dev/null 1>>"join_"$i".stats";
-        done
+        echo $i" 3 f1_"$j" f2_"$j | ../createtable 2>/dev/null 1>>"join_"$i".stats";
     done
 done
